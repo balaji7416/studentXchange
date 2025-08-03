e8f4e37e1b3d66c024acbc99e3872d4ed89f6c4d
balaji7416 <balaji.nani12345@gmail.com>
2025-08-03 16:35:57 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-green-500 capitalize leading-wide flex items-center justify-center font-bold text-2xl mt-4 shadow-sm ">
+      hello first test for Olx project
+    </h1>
+  );
+}
+
+export default App;
