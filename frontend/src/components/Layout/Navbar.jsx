ab9d99d28d3df2321cb4c3e0bb012e60c48eb12a
balaji7416 <balaji.nani12345@gmail.com>
2025-08-04 08:52:26 +0530

    search icon added to searchbar in navbar

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Input from "../UI/Input";
 import { Menu, X } from "lucide-react";
 import Avatar from "../UI/Avatar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
@@ -16,11 +18,19 @@ function Navbar() {
 
           {/* Desktop searchBar */}
           <div className="hidden md:flex items-center justify-center flex-grow">
-            <Input
-              type="text"
-              placeholder="search..."
-              className="max-w-md"
-            ></Input>
+            <div className="flex items-center justify-center w-full max-w-md rounded-md focus-within:ring-2 border-2 border-gray-200 focus-within:ring-blue-300 trasition-all ease-out duration-300">
+              <Input
+                type="text"
+                placeholder="search..."
+                className="flex-grow focus:ring-0 border-none outline-none"
+              ></Input>
+              <div className="bg-black p-2 rounded-r-md">
+                <FontAwesomeIcon
+                  icon={faSearch}
+                  className="text-white  h-full"
+                />
+              </div>
+            </div>
           </div>
 
           {/* Desktop links */}
@@ -64,8 +74,20 @@ function Navbar() {
         </div>
 
         {/* search bar for mobile */}
-        <div className="md:hidden mt-3 flex items-center justify-center px-4 py-2">
+        {/* <div className="md:hidden mt-3 flex items-center justify-center px-4 py-2">
           <Input type="text" placeholder="search..." className=""></Input>
+        </div> */}
+        <div className="md:hidden flex items-center justify-center px-3 py-1 ">
+          <div className="flex items-center justify-center w-full max-w-md rounded-md border-2 border-gray-200 focus-within:ring-2 focus-within:ring-blue-300 trasition-all ease-out duration-300">
+            <Input
+              type="text"
+              placeholder="search..."
+              className="flex-grow focus:ring-0 border-none outline-none"
+            ></Input>
+            <div className="bg-black p-2 rounded-r-md">
+              <FontAwesomeIcon icon={faSearch} className="text-white  h-full" />
+            </div>
+          </div>
         </div>
 
         {/* Mobile links */}
