5407f1d4dec4def48e46394fecb54c805e51aa88
balaji7416 <balaji.nani12345@gmail.com>
2025-08-06 23:31:52 +0530

    categories hide feature added in desktop

--- frontend/src/components/desktop/header/CategoryBar.jsx
+++ frontend/src/components/desktop/header/CategoryBar.jsx
@@ -6,8 +6,12 @@ import {
   faShirt,
   faBookOpen,
   faRuler,
+  faChevronUp,
+  faChevronDown,
+  faC,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const catagories = [
   {
@@ -43,14 +47,27 @@ const catagories = [
 ];
 
 function DesktopCatagories() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className={clsx("w-full py-2 px-3", "shadow-sm", "mt-[92px]")}>
+    <div className={clsx("w-full py-2 px-3", "shadow-sm", "mt-[92px]", "")}>
       <div className={clsx("flex items-center justify-center space-x-10")}>
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className="flex gap-2 items-center"
+        >
+          <span className="font-bold">Categories</span>
+          <FontAwesomeIcon icon={open ? faChevronDown : faChevronUp} />
+        </button>
         {catagories.map(({ name, id, icon, href }) => (
           <div
             className={clsx(
               "flex items-center gap-2 ",
-              "transition-all duration-600"
+              "transition-all duration-600",
+              open
+                ? "opacity-100 translate-x-0 scale-100 pointer-events-auto"
+                : "opacity-0 -translate-y-10 scale-95 pointer-events-none",
+              "transition-all ease-out duration-500"
             )}
             key={id}
           >
